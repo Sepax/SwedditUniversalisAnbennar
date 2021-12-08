0d120c0b75e6e405e940ca5bbcf5c0e0d83b2e6f
Sepax <sebastian@waterski.se>
2021-12-08 18:14:28 +0100

    Removed stupid defines

--- common/defines/SU-defines.lua
+++ common/defines/SU-defines.lua
@@ -1,5 +1,3 @@
------ DOGE NDefines.NDiplomacy -----
-
 --Diplo
 NDefines.NMilitary.WARGOAL_MAX_BONUS = 35
 NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
@@ -19,17 +17,8 @@ NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 0
 NDefines.NCountry.NAT_FOCUS_YEARS = 15 -- Default 25
 NDefines.NCountry.NAT_FOCUS_YEARS_RANK = 5 -- Default 5
 
---Advisor
-NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0.0075
-NDefines.NCountry.SCRIPTED_ADVISOR_DISCOUNT = 0.75
-
---Wars
-
 --Subjects
 NDefines.NCountry.BREAK_VASSAL_PRESTIGE_PENALTY = -10 -- Default -25
-
---Dynasty
-NDefines.NCountry.LEGITIMACY_DYNASTY_CHANGE = 50 -- Default 20
 
 -- For Ideas Expanded
 NDefines.NCountry.PS_BUY_IDEA = 250
@@ -45,43 +34,12 @@ NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 1 -- 2.0
 NDefines.NCountry.INNOVATIVENESS_DAYS_AFTER_FIRST_PICK_VIABLE = 60 --90
 NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 2 -- 4.0
 
--- Military
-NDefines.NCountry.LAND_FORCELIMIT_EXTRA_COST_FACTOR = 6.0
-NDefines.NCountry.NAVAL_FORCELIMIT_EXTRA_COST_FACTOR = 6.0
-NDefines.NCountry.STARTING_ARMY_SIZE = 0.75 --.75
-NDefines.NCountry.STARTING_FLEET_SIZE = 0.75 --.9
-NDefines.NCountry.PS_SCORCH_EARTH = 5
-
-
 --Estates
-NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 6		-- Max. number of privileges active at the same time
-NDefines.NCountry.ESTATE_CROWNLAND_FROM_DEV = 0.1		--0.2	-- How much crownland you gain from developing a province.
-
-
--- Parliament
-NDefines.NCountry.PARLIAMENT_ISSUE_DURATION = 10				-- in years
-NDefines.NCountry.PARLIAMENT_DEBATE_DURATION = 2			-- in years
-NDefines.NCountry.PARLIAMENT_PRESTIGE_HIT = -10			-- penalty if failed debate.
-NDefines.NCountry.PARLIAMENT_CHANCE_OF_DECISION = 10		-- % Chance debate is ended after PARLIAMENT_DEBATE_DURATION
+NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 5	-- Max. number of privileges active at the same time
 
 -- Colonialism
 NDefines.NCountry.REGULAR_COLONY_GROWTH = 35 --25
 
-
 -- Barrage
 NDefines.NCountry.PS_ARTILLERY_BARRAGE = 25
 NDefines.NCountry.PS_NAVAL_BARRAGE = 25
-
--- Great Projects
-NDefines.NCountry.TIME_TO_REPAIR_GREAT_PROJECT_ONE_PERCENT_IN_DAYS = 30		-- days to repair a great project 1%
-NDefines.NCountry.GREAT_PROJECT_FULL_STATE_OF_REPAIR = 100		-- full state of repair
-NDefines.NCountry.GREAT_PROJECT_DEGRADATION_PERCENT_PER_MONTH = 0.01 --how much a great project degrades in % per month
-NDefines.NCountry.GREAT_PROJECT_DEGRADATION_PERCENT_THRESHOLD_FOR_ALERT = 5 --below this % of repair, we'll show an alert
-NDefines.NCountry.GREAT_PROJECT_DEGRADATION_PERCENT_THRESHOLD_FOR_TIER_1 = 25 --state of repair threshold for tier 1 benefits
-NDefines.NCountry.GREAT_PROJECT_DEGRADATION_PERCENT_THRESHOLD_FOR_TIER_2 = 50 --state of repair threshold for tier 2 benefits
-NDefines.NCountry.GREAT_PROJECT_DEGRADATION_PERCENT_THRESHOLD_FOR_TIER_3 = 75 --state of repair threshold for tier 3 benefits
-NDefines.NCountry.GREAT_PROJECT_MANPOWER_REQUIRED_FOR_CONSTRUCTION_BOOST = 10000 --use a bunch of manpower to move things on
-NDefines.NCountry.GREAT_PROJECT_MANPOWER_CONSTRUCTION_BOOST_AMOUNT_IN_DAYS = 730 --how far throwing men at the problem gets you
-NDefines.NCountry.GREAT_PROJECT_MONEY_REQUIRED_FOR_CONSTRUCTION_BOOST = 500 --use a bunch of money to move things on
-NDefines.NCountry.GREAT_PROJECT_MONEY_CONSTRUCTION_BOOST_AMOUNT_IN_DAYS = 730 --how far throwing cash at the problem gets you
-NDefines.NCountry.GREAT_PROJECT_MOVE_COST_PER_DAY = 3 --how much to move a great project for one day
